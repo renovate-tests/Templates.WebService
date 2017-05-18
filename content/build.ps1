14fe1ba6318788a925b01a258bb7de986a7e3b95
Bastian Eicher <bastian.eicher@axoom.com>
2017-05-18 17:22:03 +0200

    Minor tweak

--- content/build.ps1
+++ content/build.ps1
@@ -5,7 +5,7 @@ $ImageName = "MyImageName"
 
 pushd "$(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)/src"
 
-dotnet msbuild /t:Restore /t:Build /t:Publish /p:Configuration=Release /p:Version=$Version /p:PublishDir=./obj/Docker/publish "$SolutionName.sln"
+dotnet msbuild /t:Restore /t:Build /t:Publish /p:PublishDir=./obj/Docker/publish /p:Configuration=Release /p:Version=$Version "$SolutionName.sln"
 dotnet test --configuration Release --no-build "$SolutionName.UnitTests/$SolutionName.UnitTests.csproj"
 
 docker build -t "$Registry/$MyImageName:$Version" .
