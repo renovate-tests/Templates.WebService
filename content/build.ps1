f82476f5cc11318ddb31a3b2427e271a7b3789bf
Bastian Eicher <bastian.eicher@axoom.com>
2017-05-29 14:11:14 +0200

    Fixed build script

--- content/build.ps1
+++ content/build.ps1
@@ -8,6 +8,6 @@ pushd "$(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)/src"
 dotnet msbuild /t:Restore /t:Build /t:Publish /p:PublishDir=./obj/Docker/publish /p:Configuration=Release /p:Version=$Version "$SolutionName.sln"
 dotnet test --configuration Release --no-build "$SolutionName.UnitTests/$SolutionName.UnitTests.csproj"
 
-docker build -t "$Registry/$ImageName:$Version" $SolutionName
+docker build -t "${Registry}/${ImageName}:${Version}" $SolutionName
 
 popd
