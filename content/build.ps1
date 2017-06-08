104fc1aac8827c80bb739c318ad442417325ae6a
Bastian Eicher <bastian.eicher@axoom.com>
2017-06-08 14:27:49 +0200

    Tweaked build script

--- content/build.ps1
+++ content/build.ps1
@@ -5,6 +5,7 @@ $ImageName = "my_image"
 
 pushd "$(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)/src"
 
+dotnet clean "$SolutionName.sln"
 dotnet msbuild /t:Restore /t:Build /t:Publish /p:PublishDir=./obj/Docker/publish /p:Configuration=Release /p:Version=$Version "$SolutionName.sln"
 dotnet test --configuration Release --no-build "$SolutionName.UnitTests/$SolutionName.UnitTests.csproj"
 
