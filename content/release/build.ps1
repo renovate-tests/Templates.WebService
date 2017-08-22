df2c2528a641523bdd10a697a6f3c963a414ff60
Bastian Eicher <bastian.eicher@axoom.com>
2017-08-22 12:58:52 +0200

    Tweaked release build script

--- content/release/build.ps1
+++ content/release/build.ps1
@@ -2,13 +2,12 @@ Param ([Parameter(Mandatory=$True)][string]$Version)
 $ErrorActionPreference = "Stop"
 Push-Location $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
-$DockerRegistry = $(if ($Version.Contains("-")) {"docker-ci.axoom.cloud"} else {"docker.axoom.cloud"})
-
 # Docker Compose templates
+$dockerRegistry = $(if ($Version.Contains("-")) {"docker-ci.axoom.cloud"} else {"docker.axoom.cloud"})
 foreach ($file in (Get-ChildItem -Filter *.yml.template))
 {
-  $template = Get-Content $file.FullName -Encoding "UTF8"
-  $output = $template.Replace('${DOCKER_REGISTRY}', $DockerRegistry).Replace('${VERSION}', $Version)
+  $template = Get-Content $file -Encoding "UTF8"
+  $output = $template.Replace('${DOCKER_REGISTRY}', $dockerRegistry).Replace('${VERSION}', $Version)
   $outputFile = $file.FullName.Replace(".yml.template", "-$Version.yml")
   echo "Writing $outputFile"
   $output | Out-File $outputFile -Encoding "UTF8"
@@ -17,9 +16,13 @@ foreach ($file in (Get-ChildItem -Filter *.yml.template))
 # Zero Install templates
 $released = Get-Date -Format "yyyy-MM-dd"
 $stability = if($Version.Contains("-")) {"developer"} else {"stable"}
+foreach ($file in (Get-ChildItem -Filter *.json))
+{
+  [Environment]::SetEnvironmentVariable($file.BaseName, (Get-Content $file))
+}
 foreach ($file in (Get-ChildItem -Filter *.xml.template))
 {
-   0install run http://0install.net/tools/0template.xml $file.FullName version=$Version released=$released stability=$stability
+   0install run --not-before=0.5 http://0install.net/tools/0template.xml $file.FullName version=$Version released=$released stability=$stability
 }
 
 Pop-Location
