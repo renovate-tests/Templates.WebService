a2424fa6d5615febd19876f8b0c8c3026aa4da6e
Bastian Eicher <bastian.eicher@axoom.com>
2017-10-09 18:00:22 +0200

    Use "ax release"

--- content/release/build.ps1
+++ content/release/build.ps1
@@ -2,27 +2,7 @@ Param ([Parameter(Mandatory=$True)][string]$Version)
 $ErrorActionPreference = "Stop"
 Push-Location $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
-# Docker Compose templates
 $dockerRegistry = $(if ($Version.Contains("-")) {"docker-ci.axoom.cloud"} else {"docker.axoom.cloud"})
-foreach ($file in (Get-ChildItem -Filter *.yml.template))
-{
-  $template = Get-Content $file -Encoding "UTF8"
-  $output = $template.Replace('${DOCKER_REGISTRY}', $dockerRegistry).Replace('${VERSION}', $Version)
-  $outputFile = $file.FullName.Replace(".yml.template", "-$Version.yml")
-  echo "Writing $outputFile"
-  $output | Out-File $outputFile -Encoding "UTF8"
-}
-
-# Zero Install templates
-$released = Get-Date -Format "yyyy-MM-dd"
-$stability = if($Version.Contains("-")) {"developer"} else {"stable"}
-foreach ($file in (Get-ChildItem -Filter *.json))
-{
-  [Environment]::SetEnvironmentVariable($file.BaseName, (Get-Content $file))
-}
-foreach ($file in (Get-ChildItem -Filter *.xml.template))
-{
-   0install run --not-before=0.5 http://0install.net/tools/0template.xml $file.FullName version=$Version released=$released stability=$stability
-}
+ax release my_service.yml $Version --arg DOCKER_REGISTRY=$dockerRegistry
 
 Pop-Location
