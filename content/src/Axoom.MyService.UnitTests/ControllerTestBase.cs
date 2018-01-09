0a5b5c80409608b8360169342273e5e079c604ff
Bastian Eicher <bastian.eicher@axoom.com>
2018-01-09 19:23:53 +0100

    Added base class for Controller unit tests

---
+++ content/src/Axoom.MyService.UnitTests/ControllerTestBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Axoom.MyService
+{
+    public abstract class ControllerTestBase : IDisposable
+    {
+        private readonly TestServer _server;
+
+        protected readonly HttpClient Client;
+
+        protected ControllerTestBase()
+        {
+            _server = new TestServer(new WebHostBuilder()
+                .ConfigureServices(x => x.AddRestApi())
+                .ConfigureServices(ConfigureService)
+                .Configure(x => x.UseRestApi(new HostingEnvironment())));
+            Client = _server.CreateClient();
+        }
+
+        protected abstract void ConfigureService(IServiceCollection services);
+
+        public virtual void Dispose()
+        {
+            Client.Dispose();
+            _server.Dispose();
+        }
+    }
+}
---
+++ content/src/Axoom.MyService.UnitTests/ServiceCollectionExtension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Moq;
+
+namespace Axoom.MyService
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddMock<T>(this IServiceCollection services, Mock<T> mock) where T : class
+        {
+            services.AddSingleton(mock.Object);
+            return services;
+        }
+    }
+}
