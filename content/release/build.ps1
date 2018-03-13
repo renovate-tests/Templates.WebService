453ccd88c86029721ffaf816cef8c92b13ce9183
Bastian Eicher <bastian.eicher@axoom.com>
2018-03-13 14:00:14 +0100

    Tweaked build scripts

--- content/release/build.ps1
+++ content/release/build.ps1
@@ -3,6 +3,6 @@ $ErrorActionPreference = "Stop"
 pushd $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
 $dockerRegistry = $(if ($Version.Contains("-")) {"docker-ci.axoom.cloud"} else {"docker.axoom.cloud"})
-ax release --verbose --refresh asset.yml $Version --arg DOCKER_REGISTRY=$dockerRegistry
+0install run http://assets.axoom.cloud/tools/ax.xml release --verbose --refresh asset.yml $Version --arg DOCKER_REGISTRY=$dockerRegistry
 
 popd
