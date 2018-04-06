273794da28b5c6bbf2ce8f9c0951dd2da96531e4
Bastian Eicher <bastian.eicher@axoom.com>
2018-04-06 14:33:44 +0200

    Split client into slices

---
+++ content/src/Axoom.MyService.Client/Contacts/MyServiceClient.cs
@@ -0,0 +1,10 @@
+﻿using Axoom.MyService.Contacts;
+
+// ReSharper disable once CheckNamespace
+namespace Axoom.MyService
+{
+    public partial class MyServiceClient
+    {
+        public ContactCollectionEndpoint Contacts => new ContactCollectionEndpoint(this);
+    }
+}
--- content/src/Axoom.MyService.Client/MyServiceClient.cs
+++ content/src/Axoom.MyService.Client/MyServiceClient.cs
@@ -1,18 +1,17 @@
 ﻿using System;
 using System.Net.Http;
-using Axoom.MyService.Contacts;
 using TypedRest;
 
 namespace Axoom.MyService
 {
-    public class MyServiceClient : EntryEndpoint
+    public partial class MyServiceClient : EntryEndpoint
     {
+        // NOTE: Other parts of this class are in separate slice-specific files
+
         public MyServiceClient(Uri uri) : base(uri)
         {}
 
         public MyServiceClient(Uri uri, HttpClient httpClient) : base(uri, httpClient)
         {}
-
-        public ContactCollectionEndpoint Contacts => new ContactCollectionEndpoint(this);
     }
 }
