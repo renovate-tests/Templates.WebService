b9bc5d88065b277a2bca91308ed4ffd2f21a078e
Bastian Eicher <bastian@eicher.net>
2018-04-13 20:54:44 +0200

    Renamed .sln files

--- content/src/build-dotnet.ps1
+++ content/src/build-dotnet.ps1
@@ -2,7 +2,7 @@
 $ErrorActionPreference = "Stop"
 pushd $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
-dotnet clean Axoom.MyService.NoDocker.sln
-dotnet msbuild /t:Restore /t:Build /t:Publish /p:PublishDir=./obj/Docker/publish /p:Configuration=Release /p:Version=$Version Axoom.MyService.NoDocker.sln
+dotnet clean Axoom.MyService.sln
+dotnet msbuild /t:Restore /t:Build /t:Publish /p:PublishDir=./obj/Docker/publish /p:Configuration=Release /p:Version=$Version Axoom.MyService.sln
 
 popd
