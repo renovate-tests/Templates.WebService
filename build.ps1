479cbd43c8aa7c10bab946b0ab4db024b1d5c8ed
Bastian Eicher <bastian.eicher@axoom.com>
2019-08-28 19:57:23 +0200

    Improved error handling PowerShell scripts

--- build.ps1
+++ build.ps1
@@ -3,5 +3,6 @@ $ErrorActionPreference = "Stop"
 pushd $PSScriptRoot
 
 nuget pack -Version $Version -OutputDirectory artifacts -NoPackageAnalysis
+if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
 
 popd
--- content/build.ps1
+++ content/build.ps1
@@ -3,7 +3,11 @@ $ErrorActionPreference = "Stop"
 pushd $PSScriptRoot\src
 
 dotnet clean MyVendor.MyService.sln
+
 dotnet msbuild /t:Restore /t:Build /t:Publish /p:PublishDir=./obj/Docker/publish /p:Configuration=Release /p:Version=$Version MyVendor.MyService.sln
+if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
+
 dotnet test --no-build --configuration Release --logger "junit;LogFilePath=..\..\artifacts\test-results.xml" UnitTests\UnitTests.csproj
+if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
 
 popd
